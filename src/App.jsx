61ac0d7799ff02ca51e98fb9fe1970d97e9280e1
DeNiS270806 <145908961+DeNiS270806@users.noreply.github.com>
2024-03-28 08:41:04 +0000

    ll

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,17 @@
+import { useState } from 'react';
+
 import './App.css';
 
 function App() {
+  const [cnt,setCnt]=useState(0);
+  function add(){
+    setCnt(cnt+1);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+   <header className="header">Click Counter</header>
+   <button onClick={add} className='btn'>Câcățel</button>
+   <h1 className="nrap">Nr apasari: {cnt}</h1>
     </div>
   );
 }
